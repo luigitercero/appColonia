1e59de18df2a4e9ebcfa78bc2c49ed9df8e4551b
luigitercero <luigitercero3@gmail.com>
2018-09-23 09:26:15 -0600

    funciona las animaciones

--- src/app/visual/index.js
+++ src/app/visual/index.js
@@ -1,192 +1,216 @@
-import React from 'react';
+import React, {Component} from 'react'
 import {
     Text,
+    StyleSheet,
     View,
     ListView,
     TouchableHighlight,
     Dimensions,
     Image,
-    StyleSheet,
     Animated
-} from 'react-native';
-import data from './data';
-import Header from './header'
-/**hay que agregar algo en las configuraciones de android/app/ */
-import Icon from 'react-native-vector-icons/Ionicons' //npm install react-native-vector-icons --save
+} from 'react-native'
 
-const {width,height} = Dimensions.get('window')
-export default class Visual extends React.Component {
-    static navigationOptions = {
-        title: 'new Recipes',
-        
-      };
-    constructor (props) {
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1,r2)=> r1!==r2})
+import data from './data'
+import Navbar from './navBar'
+
+const {width, height} = Dimensions.get('window')
+
+export default class Visual extends Component {
+    constructor(props){
+        super(props)
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
         this.state = {
-            isLoaded:false,
-            isOpenMenu:false,
-            dataSouce: ds.cloneWithRows(data),
+            isLoaded: false,
+            isOpenMenu: false,
+            dataSource: ds.cloneWithRows(data),
             rotateY: new Animated.Value(0),
             translateX: new Animated.Value(width),
             menuAnimation: new Animated.Value(0)
-            
-        }
-        
-    }
-    showMenu() {
-        if(this.state.isOpenMenu) {
-            this.setState({isOpenMenu:false});
-            Animated.parallel([
-                Animated.timing(
-                    this.state.translateX, {
-                        toValue:width
-                    }
-                ),/*
-                Animated.timing([
-                    this.state.rotateY, {
-                        toValue:0
-                    }
-                ])*/
-            ]).start()  //varias animaciones a la vez
-        } else {
-            this.setState({isOpenMenu:true});
-            Animated.parallel([
-                Animated.timing(
-                    this.state.translateX, {
-                        toValue:width *0.60
-                    }
-                ),/*
-                Animated.timing([
-                    this.state.rotateY, {
-                        toValue:1
-                    }
-                ])*/
-            ]).start() //varias animaciones a la vez
-            
         }
     }
-    closeMenu() {
 
+    showMenu(){
+        if(this.state.isOpenMenu){
+            this.setState({isOpenMenu: false})
+            Animated.parallel([
+                Animated.timing(
+                    this.state.translateX, {
+                        toValue: width
+                    }
+                ),
+                Animated.timing(
+                    this.state.rotateY, {
+                        toValue: 0
+                    }
+                )
+            ]).start()
+        } else {
+            this.setState({isOpenMenu: true})
+            Animated.parallel([
+                Animated.timing(
+                    this.state.translateX, {
+                        toValue: width * 0.60
+                    }
+                ),
+                Animated.timing(
+                    this.state.rotateY, {
+                        toValue: 1
+                    }
+                ),
+                Animated.timing(
+                    this.state.menuAnimation, {
+                        toValue: 1,
+                        duration: 800
+                    }
+                )
+            ]).start()
+        }
     }
-    renderRow(rowData) {
+
+    closeMenu(){
+        this.setState({isOpenMenu: false})
+        Animated.parallel([
+            Animated.timing(
+                this.state.translateX, {
+                    toValue: width
+                }
+            ),
+            Animated.timing(
+                this.state.rotateY, {
+                    toValue: 0
+                }
+            ),
+            Animated.timing(
+                this.state.menuAnimation, {
+                    toValue: 0,
+                    duration: 300
+                }
+            )
+        ]).start()
+    }
+
+    renderRow(rowData){
+        const img = rowData.image
         return (
-            <TouchableHighlight style = {styles.borederCell}>
+            <TouchableHighlight style={styles.containerCell}
+
+            >
                 <View>
-                    <Image
-                     style = {{width:width,height:180}}
-                     source =  {{uri:rowData.image}}
+                    <Image 
+                        style={{width: width, height: 180}}
+                        source={{uri: img}}
                     />
-                    <Icon name = 'ios-alarm' />
                     <View style={styles.footerContainer}>
                         <View
-                            style = {styles.imageUser}
+                            style={styles.imageUser}
                         >
-                            <Image
-                                style = {styles.imageAvatar}
-                                source={{uri:rowData.user}}
+                            <Image 
+                                style={styles.imageAvatar}
+                                source={{uri: rowData.user}}
                             />
                         </View>
                         <View style={styles.footerTextContainer}>
-                            <Text style = {styles.text}>{rowData.food}</Text>
-                            <Text style = {[styles.text,styles.textTitle]}>{rowData.title}</Text>
-                            <Text style = {[styles.text,styles.textBy]}>By {rowData.by}</Text>
+                            <Text style={styles.text}>{rowData.food}</Text>
+                            <Text style={[styles.text, styles.textTitle]}>{rowData.title}</Text>
+                            <Text style={[styles.text, styles.textBy]}>By {rowData.by}</Text>
                         </View>
                     </View>
                 </View>
             </TouchableHighlight>
         )
     }
-    render (){
+    render(){
         return (
-            <View style = {styles.container}>
+            <View style={styles.container}>
                 <Animated.View
-                    style ={[styles.content,{
-                        width:width,
-                        backgroundColor:'gray',
-                        flex:1,
+                    style={[styles.content, {
+                        width: width,
+                        backgroundColor: 'gray',
+                        flex: 1,
                         transform: [
                             {
                                 perspective: 450
                             },
                             {
-                                translateX:this.state.translateX.interpolate({
-                                    inputRange: [0,width],
-                                    outputRange: [width,0]
+                                translateX: this.state.translateX.interpolate({
+                                    inputRange: [0, width],
+                                    outputRange: [width, 0]
                                 })
                             },
-                             /*{
-                               
+                            {
                                 rotateY: this.state.rotateY.interpolate({
-                                    inputRange:[0,1],
-                                    outputRange:['0rad','1rad']
+                                    inputRange: [0, 1],
+                                    outputRange: ['0deg', '-10deg']
                                 })
-                            } */
+                            }
                         ]
                     }]}
                 >
-                    <Header icon = "bars" showMenu = {this.showMenu.bind(this)}/>
-                    <ListView style = {styles.listContainer}
-                        renderRow = {this.renderRow.bind(this)}
-                        dataSource = {this.state.dataSouce}
+                    {this.state.isOpenMenu ? <Navbar icon="times" showMenu={this.closeMenu.bind(this)}/> : <Navbar icon="bars" showMenu={this.showMenu.bind(this)}/>}
+                    <ListView 
+                        style={styles.listContainer}
+                        renderRow={this.renderRow.bind(this)}
+                        dataSource={this.state.dataSource}
                     />
                 </Animated.View>
-               
-                <Animated.View style= {{
-                    position: 'absolute',
-                    width:140,
-                    left:0,
-                    top:100,
-                    backgroundColor:'transparent'
-                }}>
-                    <Text>Home</Text>
-                    <Text>new recipes</Text>
-                    <Text>Recipes</Text>
-                    <Text>Profile</Text>
-                    <Text>Setting</Text>
-                    
-                    
+                <Animated.View
+                    style={[styles.menu, {
+                        opacity: this.state.menuAnimation,
+                        position: 'absolute',
+                        width: 140,
+                        left: 0,
+                        top: 100,
+                        backgroundColor: 'transparent'
+                    }]}
+                >
+                    <Text style={styles.textMenu}>Home</Text>
+                    <Text style={styles.textMenu}>New Recipes</Text>
+                    <Text style={styles.textMenu}>Recipes</Text>
+                    <Text style={styles.textMenu}>Profile</Text>
+                    <Text style={styles.textMenu}>Settings</Text>
                 </Animated.View>
             </View>
-            
         )
     }
 }
 
-const styles =StyleSheet.create ({
-    container:{
-        flex:1,
-
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#555566'
     },
-    content:{
+    content: {
         zIndex: 1
     },
     footerContainer: {
-        flexDirection: 'row',
-        paddingHorizontal: 10,
-        paddingVertical:10,
-        backgroundColor: '#555566'
+       flexDirection: 'row',
+       paddingHorizontal: 10,
+       paddingVertical: 10,
+       backgroundColor: '#555566' 
     },
     imageAvatar: {
         width: 50,
-        height:50,
-        borderRadius:30, 
-        marginRight:5
+        height: 50,
+        borderRadius: 25,
+        marginRight: 5
     },
     listContainer: {
-        marginHorizontal:15
+        marginHorizontal: 10
     },
     text: {
-        color:'#fff'
+        color: '#fff'
     },
-    borederCell:{
-        marginBottom:10
+    containerCell: {
+        marginBottom: 10
     },
     textTitle: {
-        fontSize:13
+        fontSize: 13
     },
-
     textBy: {
-        fontSize:12
+        fontSize: 12
+    },
+    textMenu: {
+        fontSize: 20,
+        color: '#fff'
     }
 })
